d52796139bb56870a5e474c98685dd8e5927651f
unknown <piacasiano123@gmail.com>
2018-12-08 00:04:26 -0800

    users can now set threshold

---
+++ app/components/SetLimit.vue
@@ -0,0 +1,25 @@
+<template>
+	<StackLayout  backgroundColor="white" >
+		<Label text="Set Limit:"></Label>   
+        <TextField keyboardType="number" v-model="threshold"></TextField>
+		<Button class="btn btn-outline" text="Save" @tap="save" />
+	</StackLayout>
+     
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+           threshold: this.threshold
+        };
+    },
+    methods: {
+        save() {
+            this.$store.commit('user/setThreshold', this.threshold),
+            this.$modal.close()
+        }
+    }
+};
+</script>
